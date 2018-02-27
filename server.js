3abdbf2a8b6e1f6f2a692991fd3b054239692a7a
Samuel Lawrence <sjebasingh@burning-glass.com>
2018-02-27 15:37:14 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const port = 8000;
+var fs = require("fs");
+// Get content from file
+ var contents = fs.readFileSync("./data/pastdata.json");
+ var data = JSON.parse(contents);
+
+ function GetRegularVerb(verb){
+if(verb[verb.length-1]==='e')
+return verb+'d';
+if(verb[verb.length-1]==='y')
+return verb.slice(0, verb.length-1,1)+'ied';
+ }
+
+ function PastTense(verb){
+     var response = {
+         'past tense': data[verb]? data[verb][0] : GetRegularVerb(verb),
+         'past participle': data[verb]? data[verb][1] : GetRegularVerb(verb)
+};
+return response;
+ }
+
+app.get("/:verb", function(req, res) {
+    var verb= req.params.verb;
+    res.send(PastTense(verb));
+ });
+
+ app.listen(port, function() {
+    console.log("Listening on " + port);
+  });
