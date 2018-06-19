74600e3c3c340a2c6c2f6837b226c6b27c07c2db
BGT\sjebasingh <sjebasingh@burning-glass.com>
2018-06-19 17:02:29 +0530

    Completed the module

--- script/app.js
+++ script/app.js
@@ -1,16 +1,23 @@
 var verb= ko.observable();
+var pasttense = ko.observable('');
+var participle = ko.observable('');
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function getPast(){
-    $.get( "https://pastenser.herokuapp.com/" + verb(), function(data) {
-  
-  alert(data['past tense']);
+    $.get( "https://pastenser.herokuapp.com/" + capitalizeFirstLetter(verb()), function(data) {  
+  pasttense(data['past tense']);
+  participle(data['past participle']);
 });
 }
 
 var myViewModel = {
     getPast : getPast,
-    personName: 'Bob',
-    personAge: 123,
-	verb: verb
+	verb: verb,
+	pasttense: pasttense,
+	participle: participle
 };
 
 ko.applyBindings(myViewModel);
