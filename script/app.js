79ad4cb08c65d2a6ac53b17e752dc0fe53524fee
Samuel Lawrentz <samuellawrentz@gmail.com>
2018-07-14 00:14:35 +0530

    Revamped the UI and functionality

--- script/app.js
+++ script/app.js
@@ -6,12 +6,21 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+//Make a mock data call for faster results
+(function init(){
+    $.get("https://pastenser.herokuapp.com/Load").done(() => true);
+})();
+
 function getPast(){
     $.get( "https://pastenser.herokuapp.com/" + capitalizeFirstLetter(verb()), function(data) {  
   pasttense(data['past tense']);
   participle(data['past participle']);
 });
 }
+
+$('input').on('focus', function(){
+    $(this).val('');
+});
 
 var myViewModel = {
     getPast : getPast,
